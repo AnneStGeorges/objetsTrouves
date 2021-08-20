e73ff798803f38b0abf4ffeaa0b932ff2d4f17c0
AnneStG <anne.saintgeorges@gmail.com>
2021-08-20 09:22:41 +0200

    commit du jeudi

--- app/src/main/java/com/anne/ExoAppli/CreatePostActivity.java
+++ app/src/main/java/com/anne/ExoAppli/CreatePostActivity.java
@@ -1,12 +1,16 @@
 package com.anne.ExoAppli;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
+import android.provider.MediaStore;
+import android.view.View;
 import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.RadioGroup;
@@ -17,7 +21,7 @@ import com.anne.ExoAppli.service.PostService;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.textfield.TextInputLayout;
 
-import java.util.Objects;
+
 
 public class CreatePostActivity extends AppCompatActivity {
     private String title;
@@ -28,8 +32,18 @@ public class CreatePostActivity extends AppCompatActivity {
     private String email;
     private String phoneNumber;
     private CivilityEnum gender;
-    private Button publishBtn;
-    private PostService postService = PostService.getInstance();
+    private Button publishBtn ;
+    private PostService postService;
+    private TextInputLayout titleLayout ;
+    private TextInputLayout descriptionLayout ;
+    private  TextInputLayout addressLayout ;
+    private TextInputLayout nameLayout ;
+    private TextInputLayout firstnameLayout ;
+    private  TextInputLayout emailLayout ;
+    private  TextInputLayout phoneNumberLayout;
+    private  RadioGroup genderRadioLayout;
+    private Button addPicture;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,55 +53,58 @@ public class CreatePostActivity extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
 
-
-
-
         setContentView(R.layout.activity_create_post);
-        TextInputLayout titleLayout = findViewById(R.id.addTitle);
-        TextInputLayout descriptionLayout = findViewById(R.id.description);
-        TextInputLayout addressLayout = findViewById(R.id.address);
-        TextInputLayout nameLayout = findViewById(R.id.name);
-        TextInputLayout firstnameLayout = findViewById(R.id.firstName);
-        TextInputLayout emailLayout = findViewById(R.id.email);
-        TextInputLayout phoneNumberLayout = findViewById(R.id.phoneNumber);
-        RadioGroup genderRadioLayout = findViewById(R.id.genderRadio);
+       postService = PostService.getInstance();
+        titleLayout = findViewById(R.id.addTitle);
+       descriptionLayout = findViewById(R.id.description);
+           addressLayout = findViewById(R.id.address);
+      nameLayout = findViewById(R.id.name);
+     firstnameLayout = findViewById(R.id.firstName);
+         emailLayout = findViewById(R.id.email);
+          phoneNumberLayout = findViewById(R.id.phoneNumber);
+       genderRadioLayout = findViewById(R.id.genderRadio);
+         addPicture = findViewById(R.id.addPicture);
+            addPicture.setOnClickListener(this::onAddPostClicked);
+        addPicture = findViewById(R.id.addPicture);
+        addPicture.setOnClickListener(this::onPickPictureFromGallery);
 
-        publishBtn = findViewById(R.id.publish);
-        publishBtn.setOnClickListener(view -> {
-           title = titleLayout.getEditText().getText().toString();
-           description = descriptionLayout.getEditText().getText().toString();
-           address = addressLayout.getEditText().getText().toString();
-           name = nameLayout.getEditText().getText().toString();
-           firstname = firstnameLayout.getEditText().getText().toString();
-           email = emailLayout.getEditText().getText().toString();
-           phoneNumber = phoneNumberLayout.getEditText().getText().toString();
-           genderRadioLayout.setOnCheckedChangeListener(((radioGroup, checkedId) ->{
-                switch(checkedId){
-                    case R.id.radioWoman: gender = CivilityEnum.MRS;
-                        break;
-                    case R.id.radioMan: gender = CivilityEnum.MR;
-                        break;
-                }
-            }));
+    }
+
+    private void onAddPostClicked(View view){
+        title = titleLayout.getEditText().getText().toString();
+        description = descriptionLayout.getEditText().getText().toString();
+        address = addressLayout.getEditText().getText().toString();
+        name = nameLayout.getEditText().getText().toString();
+        firstname = firstnameLayout.getEditText().getText().toString();
+        email = emailLayout.getEditText().getText().toString();
+        phoneNumber = phoneNumberLayout.getEditText().getText().toString();
+        genderRadioLayout.setOnCheckedChangeListener(((radioGroup, checkedId) ->{
+            switch(checkedId){
+                case R.id.radioWoman: gender = CivilityEnum.MRS;
+                    break;
+                case R.id.radioMan: gender = CivilityEnum.MR;
+                    break;
+            }
+        }));
+
             Post post = new Post(title, description, address, gender, name, firstname, email, phoneNumber);
             postService.createPost(post);
 
             //snackbar (success message)
             Snackbar snackbar = Snackbar.make(view, R.string.post_created_successfully, Snackbar.LENGTH_SHORT);
-                snackbar.addCallback(new Snackbar.Callback(){
-                    @Override
-                    public void onDismissed(Snackbar transientBottomBar, int event){
-                        if (event ==Snackbar.Callback.DISMISS_EVENT_TIMEOUT){
-                            finish();
-                        }
+            snackbar.addCallback(new Snackbar.Callback(){
+                @Override
+                public void onDismissed(Snackbar transientBottomBar, int event){
+                    if (event ==Snackbar.Callback.DISMISS_EVENT_TIMEOUT){
+                        finish();
                     }
-                });
-                snackbar.show();
+                }
+            });
+            snackbar.show();
 
-        });
-    }
+        };
 
-    //method to go back to previous page
+        //method to go back to previous page
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()) {
@@ -95,5 +112,28 @@ public class CreatePostActivity extends AppCompatActivity {
             break;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode != RESULT_CANCELED) {
+
+            switch (requestCode) {
+                case REQUEST_CODE_PICK_PICTURE:
+                    if (resultCode == RESULT_OK && data != null) {
+                        Uri selectedPicture = data.getData();
+                        imageViewPicture.setImageURI(selectedPicture);
+                    }
+                    break;
+            }
+
+        }
+    }
+
+    private void onPickPictureFromGallery(View view) {
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, REQUEST_CODE_PICK_PICTURE);
     }
 }
--- app/src/main/java/com/anne/ExoAppli/DetailsPostActivity.java
+++ app/src/main/java/com/anne/ExoAppli/DetailsPostActivity.java
@@ -1,23 +1,39 @@
 package com.anne.ExoAppli;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 
+import android.text.Html;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 import com.anne.ExoAppli.model.Post;
 
 public class DetailsPostActivity extends AppCompatActivity {
+private Post post;
+
+    public DetailsPostActivity(Post post) {
+        this.post = post;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details_post);
+
+        //set actionbar as visible
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
         TextView tvName = findViewById(R.id.activity_details_item_title);
         TextView tvDescription = findViewById(R.id.activity_details_item_description);
-        TextView tvAdresse = findViewById(R.id.activity_details_item_adress);
+        TextView tvAddress = findViewById(R.id.activity_details_item_address);
+
 
         // Get bundle post
         Intent intent = getIntent();
@@ -25,7 +41,48 @@ public class DetailsPostActivity extends AppCompatActivity {
             Post post = intent.getParcelableExtra(ViewListActivity.KEY_BUNDLE_POST);
             tvName.setText(post.getName());
             tvDescription.setText(post.getDescription());
-            tvAdresse.setText(post.getAddress());
+            tvAddress.setText(post.getAddress());
         }
+    }
+
+    //to display the share button
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_details_post, menu);
+        return true;
+    }
+
+    //method to go back to previous page
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home : finish();
+                break;
+            case R.id.action_share: share(post);
+            break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+    //share by SMS
+    private void share(Post post) {
+        Intent intent = new Intent(android.content.Intent.ACTION_SEND);
+        intent.setType("text/plain");
+
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("<p>Salut ! J'ai trouvé une annonce importante sur Retrouvez-moi... :</p>");
+        sb.append("<p>Nom : ").append(post.getName()).append("</p>");
+        sb.append("<p>Description : ").append(post.getDescription()).append("</p>");
+        sb.append("<p>Si vous trouvez, appelez le ").append(post.getPhoneNumber()).append("</p>");
+
+        String content = Html.fromHtml(sb.toString()).toString();
+
+        intent.putExtra(android.content.Intent.EXTRA_SUBJECT, post.getName());
+        intent.putExtra(android.content.Intent.EXTRA_TEXT, content);
+
+        startActivity(Intent.createChooser(intent, getString(R.string.share_with)));
     }
 }
